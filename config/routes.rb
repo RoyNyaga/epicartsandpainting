6ac36adac554e05b356711729f049c91a36e8c32
Roy Nyaga <nyagaandreroy@gmail.com>
2020-05-17 05:52:10 +0100

    add pricing page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'pages/about', as: "about"
   get 'pages/services', as: "services"
+  get 'pages/pricing', as: "pricing"
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
